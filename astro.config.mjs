2da721217dd09c6a52e5bdf38c37700a400092c6
Hamza Ali <hamzaa1i@users.noreply.github.com>
2026-03-20 08:23:27 +0500

    v1.6: content cleanup, case study links, sitemap, fix dates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://hamzaali-dev.vercel.app',
+  site: 'https://hamzaalidev.vercel.app',
   output: 'static',
 
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
+    sitemap(),
   ],
 
   vite: {
